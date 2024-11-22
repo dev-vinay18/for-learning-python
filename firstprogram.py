1dbc8c56e587926d4e262b976c09234eaa239aa8
vinay wattamwar <vinaywattamwar780@gmail.com>
2024-11-22 10:37:58 +0530

    commit message

---
+++ firstprogram.py
@@ -0,0 +1,51 @@
+# arithmetic operators
+# a = 5
+# b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+
+#assignmet operators
+# num = 10
+# num **= 10
+# print("num :" , num)
+
+#logical operators
+# val1 = False
+# val2 = False
+
+
+# print("AND operator :" , val1 and val2)
+# print("OR operator :" , val1 or val2)
+
+# a = 30
+# b = 40
+
+# print (not False)
+# print (not (a > b))
+
+# name = input("enter name:")
+# age = int(input("enter age: "))
+# marks = float(input("enter age :"))
+
+# print("welcome" , name)
+# print(type(age) , "your age :", age)
+# print(type(marks) , "your marks :", marks)
+
+# first = int(input("enter first :"))
+# secound = int(input("enter second :"))
+
+# print( "sum =" , first + secound )
+
+# side = float(input("enter the side :"))
+
+# print(type(side) ,"area =" , side * side)
+
+a = int(input("firt int number:"))
+b = int(input("second int number:"))
+
+print("the ans." , a >= b )                                         
---
+++ secondlec.py
@@ -0,0 +1,106 @@
+# str1 = "My"
+# len1= len(str1)
+# str2 = "college"
+# len2=len(str2)
+# finalstr = (str1+ " " +str2)
+# print(finalstr)
+# print(len(str1))
+# print(len2)
+# print(len(finalstr)) 
+
+# str1 = "this is a string.\n we are creating in pyton."
+# print(str1)
+
+# str2 = "this is a string.\t we are creating in pyton."
+# print(str2) 
+
+# str = "google"
+# ch = str[4]
+# print(ch)
+
+# str = "temperature"
+# print(str[4])
+  
+# str = " my repository"                      # slicing
+# print(str[0 : 5])
+# print(str[2:len(str)])
+# print(str[ :3])
+# print(str[3:])
+
+# str = "Apple"                              # negative slicing
+# print(str[-5:-2])
+
+# str = "i am a coder"                          # string functions
+# print(str.endswith("er"))
+# str = str.capitalize()
+# print(str)
+# print(str.replace("coder" , "singer"))
+# print(str.find("a"))
+# print(str.count("z")) 
+
+# a = input("first name :")                   # practice question
+# print("length of your name :" , len(a))
+
+# str = " gote $ b $ cat $"
+# print(str.count("$")) 
+
+# light = "ghchgds"                           # conditional statement
+
+# if(light == "red") :
+#     print("stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light=="yellow"):
+#     print("look")
+# else:
+#     print("light is broken")
+
+# print("end of code.")
+
+# marks = int(input("marks ;"))            # question for conditional statement
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+
+# print(" grade of student :" , grade) 
+
+# age = 6
+
+# if(age >= 18):
+#     if( age >= 80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+# num = int(input("enter value : "))              # practice questions
+
+# if(num % 2 == 0) :
+#     print("even")
+# else:
+#     print("odd")
+
+# a = int(input("enter first value : "))
+# b = int(input("enter secound value : "))
+# c = int(input("enter third value : "))
+
+# if(a >= b and a >= c):
+#     print("first number is largest :" , a)
+# elif(b >= c ):
+#     print("secound number is largest :" , b)
+# else:
+#     print("third number is largest :" , c)
+
+# num = int(input("digit :"))
+
+# if(num % 7 == 0) :
+#     print("yes")
+# else:
+#     print("no")
