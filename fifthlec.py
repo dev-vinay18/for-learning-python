13f575ac53bb0b189622d9d237165b6f931efe8b
vinay wattamwar <vinaywattamwar780@gmail.com>
2024-12-10 11:10:58 +0530

    new

---
+++ fifthlec.py
@@ -0,0 +1,48 @@
+########################## LOOPS ##############################
+
+# count = 1                                                 # in loops they repeat instructions. (thats why we use condition after 'while')
+# while count <= 5 :                                        # and this repeating called as Iteration.
+#     print("arnav")
+#     count += 1 
+
+# i = 1
+# while i <= 5 :
+#     print("gore")
+#     i += 1
+
+# j = 1                                                       # print numbers from 1 to 5. Q
+# while j <= 5 :
+#     print(j)
+#     j += 1
+
+# print("loop ended")
+
+# k = 5                                                       # print numbers from 5 to 1. Q
+# while k >= 1 :
+#     print(k)
+#     k -= 1
+
+# print("loop ended.")
+
+# m = 1                                                         # print numbers from 1 to 100. Q
+# while m <= 100 :
+#     print(m)
+#     m += 1
+
+# n = 100                                                      # print numbers from 100 to 1. Q
+# while n >= 1 :
+#     print(n)
+#     n -= 1   
+
+# n = int(input("enter value : "))                                 # print the multiplication number of n. Q
+# y = 1
+# while y <= 10 :
+#     print(n*y)
+#     y += 1
+
+nums = [1,4,9,16,25,36,49,64,81,100]                             #print the elements of the folowing number using loop. Q
+
+idx = 0 
+while idx < len(nums) :
+    print(nums[idx])
+    idx += 1
