7b5314c41bc687b65ff7cb6b306ff23d5854da76
vinay wattamwar <vinaywattamwar780@gmail.com>
2024-12-24 12:48:59 +0530

    commit 2nd

--- lecseven.py
+++ lecseven.py
@@ -6,10 +6,14 @@ f = open("demo.txt","r")
 # data = f.read()                     # for reading entire file.
 # print(data)
 
-line1 = f.readline()                 # read 1 linwe at 1 time.
-print(line1)
+# data = f.read(9)                       # we can also pass an argument(index value) for our need.
+# print(data)
 
-line2 = f.readline()
-print(line2)
+# line1 = f.readline()                 # read 1 linwe at 1 time.
+# print(line1)
+
+# line2 = f.readline()
+# print(line2)
+
 
 f.close()
