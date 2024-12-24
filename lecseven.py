c108ae8353bf7f7e49640e7abfb8f7bb3f511aa6
vinay wattamwar <vinaywattamwar780@gmail.com>
2024-12-24 12:24:14 +0530

    1st commit

--- lecseven.py
+++ lecseven.py
@@ -1,5 +1,15 @@
 ###### File I/O in python #####
 f = open("demo.txt","r")
-data = f.read()
-print(data)
-print(type(data))
+
+# print(type(data))
+
+# data = f.read()                     # for reading entire file.
+# print(data)
+
+line1 = f.readline()                 # read 1 linwe at 1 time.
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+f.close()
