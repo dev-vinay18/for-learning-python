276a996fb78abe79b0dd33dfaee7863a2bc90ab6
vinay wattamwar <vinaywattamwar780@gmail.com>
2024-12-28 19:44:27 +0530

    new

--- lecseven.py
+++ lecseven.py
@@ -11,7 +11,7 @@
 # data = f.read(9)                       # we can also pass an argument(index value) for our need.
 # print(data)
 
-# line1 = f.readline()                 # read 1 linwe at 1 time.
+# line1 = f.readline()                 # read 1 line at 1 time.
 # print(line1)
 
 # line2 = f.readline()
@@ -19,10 +19,26 @@
 
 # f.close()
 
-## "w" = for writing to a file
+## "w" = for writing to a file                 # in write & append mode if we open any non existed file then thet file will be opened automatically. 
 
-f = open("demo.txt","w")
+# f = open("demo.txt","w")
 
-f.write("new")
+# f.write("new")
 
 ## "a" = for adding 
+
+# f = open("demo.txt","a")
+
+# f.write("\nwe will learn from tommarow.")
+
+# f.close()
+
+## "r+" = for over writting means characteres from first started replacing which we will write in write mode.
+ 
+# f = open("demo.txt","r+")
+
+# f.write("abc")
+
+# f.close()
+
+
