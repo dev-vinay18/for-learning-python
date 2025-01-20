ecad32063e38568222230465324542162656a73b
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-01-20 19:13:23 +0530

    add

--- lecseven.py
+++ lecseven.py
@@ -106,7 +106,33 @@
 
 # check_for_word("new")
 
-def check_for_line():
-    word = "learning"
-    data = True
-    line_no = 1
+## Q
+# def check_for_line(word):
+#     # word = "learning"
+#     data = True
+#     line_no = 1
+#     with open("practice.txt","r") as f:
+#         while data :
+#             data = f.readline()
+#             if(word in data):
+#                 print(line_no)
+#                 return
+#             line_no += 1
+    
+#     return -1
+
+# print(check_for_line("programming"))
+
+## Q
+count = 0
+with open ("demo.txt", "r") as f:
+    data = f.read()
+
+    nums = data.split(",")
+    for i in nums:
+        if(int(i) % 2 == 0):
+            count += 1
+    
+    print(count)
+
+        
