2f8e48a5d243180246e418617581a72905b054be
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-01-23 20:46:30 +0530

    is

--- eighthlec.py
+++ eighthlec.py
@@ -26,18 +26,53 @@
 ## constructor = object creation 
 ## All classes have function _init_(), which is always executed when the object is being intiated.
 
+# class student:
+#     def __init__(self, name , marks):
+#         self.name = name
+#         self.marks = marks
+#         print("This function is added now..")
+
+# N1 = student("tejas", 20)
+# print(N1.name)
+# print(N1.marks)
+
+# N2 = student("arnav", 38)
+# print(N2.name, N2.marks)
+
+# class student:
+#     college_name = "ABC college"
+
+#     def __init__(self, name , marks):
+#         self.name = name
+#         self.marks = marks
+
+#     def welcome(self):
+#         print("welcome student:", self.name)
+
+#     def get_marks(self):
+#         return self.marks
+    
+# s1 = student("arnav" , 35)
+# s1.welcome()
+# print(s1.get_marks())
+
+## Q
+
 class student:
-    def __init__(self, name , marks):
+
+    def __init__(self,name,marks):
         self.name = name
         self.marks = marks
-        print("This function is added now..")
 
-N1 = student("tejas", 20)
-print(N1.name)
-print(N1.marks)
+    def avg_marks(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("hi", self.name, "your avg score in list :" , sum/3)
 
-N2 = student("arnav", 38)
-print(N2.name, N2.marks)
-
-
+s1 = student("arnav", [77,67,39])
+s1.avg_marks()
         
+
+
+           
