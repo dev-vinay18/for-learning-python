0b9c70d07e49fa1c8135deb7e88046fc3877563c
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-05-21 19:38:15 +0530

    w

--- c++/code.cpp
+++ c++/code.cpp
@@ -1,7 +1,98 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "new";
-    return 0; 
-}
+// int main() {
+//     int age = 20;
+//     char name = 'b';
+//     float percent = 90.08f;
+//     bool itis = false;          // If value is true -> 1 & If false -> 0
+//     double value = 100.98;
+   
+//     return 0; 
+// }
+
+// int main() {              // Type casting
+//     double grade = 134.56;
+//     int oldgrade = (int)grade;
+//     cout << oldgrade << endl;
+//     return 0;
+// }
+
+// int main() {
+//     int age;
+
+//     cout << "enter the age : " ;
+//     cin >> age ;
+
+//     cout << "you enterd age  is : " << age << endl;
+//     return 0;
+// }
+
+// int main()  {                                    // arethmetic operators
+//     int a = 21 , b = 4;
+
+    // cout << "sum = " << (a+b) << endl;
+    // cout << "subtraction = " << (a-b) << endl;
+    // cout << "product =" << (a*b) << endl;
+    // cout << "division =" << (a/b) << endl;
+    // cout << "modulo =" << (a%b) << endl;
+
+//     cout << (5 /(double) 2) << endl;
+//     return 0;
+// }
+
+// int main() {                                     // rational operators
+//     cout << (6 > 7) << endl; // false = 0
+//     cout << (6 < 7) << endl; // true = 1
+//     cout << (4 <= 4) << endl; // true = 1
+//     cout << (6 >= 7) << endl; // false = 0
+//     cout << (6 != 7) << endl; // true = 1 
+//     cout << (6 == 7) << endl; // false = 0 
+//     return 0;
+// }
+
+// int main() {                                // logical operators
+//     cout << !(6 < 7) << endl; // in this ! gives the opposie value of the operation that we are performing. 
+//     cout << ((3<4)||(6>9)) << endl;  // "||" this is or operator in this one of the value is true in given operations is true or both the values are true then operation is true
+//     cout << ((5<6)&& (8<3)) << endl; // "&&" this is and operator in this both the operations must true other wise the operator gives false 
+//     return 0;
+// }
+
+// int main() {
+//     int a, b;
+//     cout << "enter a :" ;
+//     cin >> a; 
+//     cout << "enter b :" ;
+//     cin >> b;
+
+//     cout << "sum :" << (a + b) << endl;
+//     return 0;
+// }
+
+// int main() {                                    // unary operators  
+//     int a = 10;                                  // 1st type Increment ++ 
+//     int b = a++;                                 // a++ --> kaam ; update
+
+//     cout << "b = " << b << endl;
+//     cout << "a = " << a << endl;
+
+//     int a = 10;                                  // ++a --> update ; kaam
+//     int b = ++a;
+
+//     cout << "b = " << b << endl;
+//     cout << "a = " << a << endl;
+
+//     int a = 10;                                  // 2nd type Decrement --
+//     int b = a--;                                 // a-- --> kaam ; update
+
+//     cout << "b = " << b << endl;
+//     cout << "a = " << a << endl;
+
+//     int a = 10;                                  // --a --> update ; kaam
+//     int b = --a;
+
+//     cout << "b = " << b << endl;
+//     cout << "a = " << a << endl;
+//     return 0;
+    
+// }
