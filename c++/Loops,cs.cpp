0c01414535b32c0b16edb1369afdcaa1b6dd4678
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-05-23 17:38:51 +0530

    ok

--- c++/Loops,cs.cpp
+++ c++/Loops,cs.cpp
@@ -14,22 +14,51 @@ using namespace std;
 //     return 0;
 // } 
 
-int main()  {                                       // Q. for gatting grade A,B,C .
-    int marks;
-    cout << "enter marks :";
-    cin >> marks;
+// int main()  {                                       // Q. for gatting grade A,B,C .
+//     int marks;
+//     cout << "enter marks :";
+//     cin >> marks;
 
-    if(marks >= 90)  {
-        cout << "A\n";
-    } else if (marks >= 80 && marks <90) {
-        cout << "B\n";
-    } else {
-        cout << "C\n";
+//     if(marks >= 90)  {
+//         cout << "A\n";
+//     } else if (marks >= 80 && marks <90) {
+//         cout << "B\n";
+//     } else {
+//         cout << "C\n";
+//     }
+
+//     return 0;
+// }
+
+// int main() {                                            // find char lowercase or uppercase
+//     char ch;
+//     cout << "enter ch : ";
+//     cin >> ch;
+
+//     if(ch >= 'a' && ch <= 'z') {
+//         cout << "lowercase\n";
+//     } else {
+//         cout << "uppercase\n";
+//     }
+// }
+
+// int main(){                                                 // Ternary Operator / statements
+//     int n = 23;
+
+//     cout << (n >= 0 ? "positive" : "negative") << endl;
+//     return 0;
+
+// }
+
+int main() {                                                    // while loop
+    int n = 10;
+    int count = 1;
+
+    while (count <= n ) {
+        cout << count << " ";
+        count++;
     }
-
+    cout << endl;
     return 0;
+    
 }
-
-
-
-
