7174b9fcf788249e5d6d5112b6b58936567c2b24
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-06-17 09:28:11 +0530

    ff

--- c++/Loops,cs.cpp
+++ c++/Loops,cs.cpp
@@ -275,14 +275,55 @@ using namespace std;
 //     return 0;
 // }
  
- int main() {
-    int n = 5;
-    for (int i = 1 ; i <= n ; i++ ){
-        int m = 5;
-        for (int j = 1 ; j <= m; j++){
-            cout << "*";
+//  int main() {
+//     int n = 5;
+//     for (int i = 1 ; i <= n ; i++ ){
+//         int m = 5;
+//         for (int j = 1 ; j <= m; j++){
+//             cout << "*";
+//         }
+//         cout << endl; 
+//     }
+//     return 0;
+//  }
+
+// int main(){                                      // nested loop for 1 to n numbers.
+//     int n = 4;
+
+//     for(int i = 1; i <= n; i++){
+
+//         for(int j = 1; j <= n; j++){
+//             cout << j << "";
+//         }
+//         cout << endl;
+//     }
+
+// }
+
+// int main(){                                      // star pattern by ussing nasted loop.
+//     int n = 4;
+
+//     for(int i = 1; i <= n; i++){
+
+//         for(int j = 1;j <= n; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+int main(){
+    int n = 4;
+
+    for(int i = 0; i < n; i++){
+        char ch = 'A';
+        
+        for(int j = 0; j < n; j++){
+            cout << ch << " ";
+            ch = ch + 1;
         }
-        cout << endl; 
+        cout << endl;
     }
     return 0;
- }
+}
