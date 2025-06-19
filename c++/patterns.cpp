327c7dbe72c606f4bcce110c7adb2921d58cecc3
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-06-19 21:39:22 +0530

    ww

--- c++/patterns.cpp
+++ c++/patterns.cpp
@@ -33,20 +33,76 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    int n = 3;
-    char ch = 'A';
-    int h = 0;
+// int main(){
+//     int n = 3;
+//     char ch = 'A';
+//     int h = 0;
 
-    for(int i= 0;i < n; i++){
-        for(int j = 0;j < n; j++){
-            // char ch = ch + 1;
-            char ne = ch + h;
-            cout << ne << " " ;
-            h++;
+//     for(int i= 0;i < n; i++){
+//         for(int j = 0;j < n; j++){
+//             // char ch = ch + 1;
+//             char ne = ch + h;
+//             cout << ne << " " ;
+//             h++;
             
-        }
-        cout << '\n';
-    }
+//         }
+//         cout << '\n';
+//     }
 
+// }
+
+// int main() {
+//     int n = 4;
+
+//     for(int i = 0;i < n ;i++){
+//         for (int j = 0;j < i+1;j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// int main () {                                                // question for practice.
+//     int n = 4;
+//     // char ch = '1';
+//     // int t = 1;
+
+//     for(int i = 1;i <= n;i++){
+//         for(int j = 1;j <= n;j++){
+//             // int t = 1;
+//             cout << j << " ";
+//             // t++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// int main () {
+//     int n = 4;
+
+//     for(int i = 0;i < n;i++){
+//         for(int j = 0;j < i + 1;j++){
+//             cout << (i+1) << " ";
+//         }
+//         cout << endl ;
+//     }
+//     return 0;
+// }
+
+int main(){
+    int n = 4;
+    char ch = 'A';
+    int t = 0;
+
+    for(int i = 0;i < n;i++){
+        char y = ch + t;
+        for(int j = 0;j < i + 1;j++){
+            cout << y ;
+            t++;
+        }
+        cout << "\n";
+    }
+    return 0;
 }
