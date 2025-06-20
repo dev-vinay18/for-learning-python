4315a14504e11ac7e9cc2a5afd4ff5a90b300aea
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-06-20 15:27:43 +0530

    dd

--- c++/patterns.cpp
+++ c++/patterns.cpp
@@ -91,18 +91,45 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    int n = 4;
-    char ch = 'A';
-    int t = 0;
+// int main(){
+//     int n = 5;
+//     char ch = 'A';
+//     int t = 0;
 
-    for(int i = 0;i < n;i++){
-        char y = ch + t;
-        for(int j = 0;j < i + 1;j++){
-            cout << y ;
-            t++;
-        }
-        cout << "\n";
-    }
-    return 0;
-}
+//     for(int i = 0;i < n;i++){
+//         char y = ch + t;
+//         t++;
+//         for(int j = 0;j < i + 1;j++){
+//             cout << y << " ";
+//             }
+//         cout << "\n";
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n = 4;
+
+//     for(int i = 0;i < n;i++){
+//        { for(int j = 0 ;j < i+1; j++)
+//         cout << (j+1) << " " ;
+//        }
+//        cout << endl;
+
+//     }
+//     return 0;
+// }
+
+// int main (){
+//     int n = 4;
+
+//     for (int i = 0;i < n; i++){
+//         for(int j = 1;j <= i+1;j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
