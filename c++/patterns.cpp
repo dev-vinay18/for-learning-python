f3c6dbd4c6701ca60fb9b80666990a12968b1247
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-06-20 16:06:33 +0530

    ff

--- c++/patterns.cpp
+++ c++/patterns.cpp
@@ -132,4 +132,16 @@ using namespace std;
 //     return 0;
 // }
 
+int main(){
+    int n = 4;
+
+    for(int i = 0; i < n;i++){
+        for(int j = i+1;j > 0;j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
 
