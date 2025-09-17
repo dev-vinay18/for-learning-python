5f35daee60dfbc37c239fa5faa4e3754d9a49832
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-09-17 15:10:08 +0530

    new

--- c++/patterns.cpp
+++ c++/patterns.cpp
@@ -155,19 +155,59 @@ using namespace std;
 //     }    
 // }
 
-int main(){
-    int n = 4;
+// int main(){
+//     int n = 4;
     
 
-    for(int i = 0; i < n; i++){
-        char ch = 'A';
-        for(int j = 0; j < n;j++){
-            cout << ch << " ";
-            ch+=1;
-        }
-        cout << endl;
-    }
-    return 0;
+//     for(int i = 0; i < n; i++){
+//         char ch = 'A';
+//         for(int j = 0; j < n;j++){
+//             cout << ch << " ";
+//             ch+=1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
 
-}
+// }
 
+// int main() {
+//     int n = 3;
+//     char ch ='A';
+
+//     for(int i = 0; i < n;i++){
+       
+//         for(int j = 0;j < n;j++){
+//             cout << ch << " ";
+//             ch+=1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int n = 3;
+
+//     for(int i = 0; i < n;i++){
+//         for(int j = 0; j < n;j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int n = 4;
+//     int num = 1;
+
+//     for(int i = 1; i <= n;i++){
+//         for(int j = 1;j <= n;j++){
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
