79be5f7f2509e1a65194e64b9b56991cf6e11fcb
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-09-21 14:27:57 +0530

    ho

--- c++/patterns.cpp
+++ c++/patterns.cpp
@@ -329,3 +329,37 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+// int main() {
+//     int n = 4;
+
+//     for (int i = 0;i < n;i++){
+//         for (int j= 0;j < i;j++){
+//             cout << " ";
+//         }
+//         for (int j = 0;j < n-i;j++){
+//             cout << (i+1);
+//         }
+//         cout <<endl;
+//     }
+//     return 0;
+// }
+
+int main() {
+    int n = 4;
+    char ch = 'A';
+
+    for (int i = 0;i < n;i++){
+       
+        for(int j = 0;j < i;j++){
+            cout << " ";
+            }
+        for(int j = 0;j < n-i;j++){
+            cout << ch;
+            // ch++;
+            }
+        ch++;
+        cout << endl;
+        
+    }
+}
