cbd2ffc639dd73907fb34699fe0ea00103aec94b
vinay wattamwar <vinaywattamwar780@gmail.com>
2025-10-23 18:16:14 +0530

    jj

--- c++/functions.cpp
+++ c++/functions.cpp
@@ -46,26 +46,50 @@ using namespace std;
 //     cout << SumofdDigit(2345) << endl;
 // }
 
-int factorial(int n){
+// int factorial(int n){
 
-    int facN = 1;
-    for (int i = 1;i <= n;i++){
-        facN *= i;
-    }
-    return facN;
-}
+//     int facN = 1;
+//     for (int i = 1;i <= n;i++){
+//         facN *= i;
+//     }
+//     return facN;
+// }
 
-int nCr(int n ,int r){
+// int nCr(int n ,int r){
 
-    int fac_n = factorial(n);
-    int fac_r = factorial(r);
-    int fac_nmr = factorial(n-r);
+//     int fac_n = factorial(n);
+//     int fac_r = factorial(r);
+//     int fac_nmr = factorial(n-r);
 
-    return fac_n / (fac_r * fac_nmr);
+//     return fac_n / (fac_r * fac_nmr);
 
-}
+// }
 
-int main (){
-    int n = 8, r = 2;
-    cout << nCr(n,r) << endl;
-}
+// int main (){
+//     int n = 8, r = 2;
+//     cout << nCr(n,r) << endl;
+// }
+
+// int primenum(int n){
+//     bool isprime = true ;
+
+//     for (int i = 2; i < n-1;i++){
+//         if(n % i == 0){
+//             isprime = false;
+//             break;
+//         }
+//         // return 0;
+//     }
+//     if(isprime == false){
+//         cout << "non prime\n ";
+//     }else{
+//         cout << "prime\n";
+//     }
+//     return 0;
+// }
+
+// int main(){
+    
+//     cout << primenum(67) << endl;
+// }
+
